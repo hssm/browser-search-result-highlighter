f7801b50461adbf36a4701ce11e49555d6140b90
hssm <houssam.salem.au@gmail.com>
2024-12-18 17:22:48 +1100

    Refactor: move out parser to utils

--- browserplus/core.py
+++ browserplus/core.py
@@ -8,6 +8,9 @@ from aqt.browser import SearchContext
 from aqt.editor import Editor, EditorMode
 from aqt.webview import WebContent
 from aqt.theme import theme_manager
+
+from browserplus.utils.parser import parse_search
+
 addon_package = mw.addonManager.addonFromModule(__name__)
 
 class BrowserPlus:
@@ -29,8 +32,7 @@ class BrowserPlus:
 
     def did_search(self, ctx: SearchContext):
         """Search has happened (regardless of source). Do highlighting."""
-        terms = get_search_terms(ctx.search)
-        self.filter_terms = extract_searchable_terms(terms)
+        self.filter_terms = parse_search(ctx.search)
         print("BP: didSearch: Highlighting these terms: ", self.filter_terms)
 
     def _column_data(self, item, is_notes_mode, row, active_columns):
@@ -56,14 +58,28 @@ class BrowserPlus:
                     self.browser.form.searchEdit.setStyleSheet("QWidget{background: #ffc9b9}")
                 # Fake a search to remove previous highlights as current search is not valid
                 self.filter_terms = []
-                did_load_note(self.editor)
+                self.did_load_note(self.editor)
 
     def on_current_index_changed(self, index):
         """Do a search on drop-down selection. -1 is text edit. Skip those as we handle already"""
         if index >= 0:
             self.on_text_edited()
 
+    def on_webview_will_set_content(self, web_content: WebContent, context):
+        if not isinstance(context, Editor):
+            return
+        web_content.js.append(f"/_addons/{addon_package}/web/editor.js")
 
+    def did_load_note(self, editor, focusTo=None) -> None:
+        if editor.editorMode is EditorMode.BROWSER:
+            # TODO: is there a better way to escape everything?
+            terms = json.dumps(browser_plus.filter_terms)
+            terms = terms.replace("'", r"\'")
+            terms = terms.replace("\\\\", r"\\\\")
+            terms = terms.replace("|", r"\\\\|")
+            editor.web.eval(f"bp_terms_str = '{terms}'")
+            editor.web.eval(f"bpParseTerms()")
+            editor.web.eval("bpHighlightAll()")
 
 class FilterHighlightDelegate(QStyledItemDelegate):
     def __init__(self, owner, choices):
@@ -111,133 +127,17 @@ class FilterHighlightDelegate(QStyledItemDelegate):
             return QStyledItemDelegate.paint(self, painter, option, index)
 
 
-# Split search string by search terms
-def get_search_terms(search):
-    terms = []
-
-    start = 0
-    close = None
-    level = 1
-    backtrack = 0
-    idx = -1
-    for c in search:
-        idx += 1
-        # We're looking for a terminator
-        if close:
-            s_from = None
-            s_to = None
-
-            if close == ')' and c == '(':
-                level += 1
-                continue
-            if close == ')' and c == ')':
-                level -= 1
-                if level > 0:
-                    continue
-
-            if ((c == '"' and close == '"')
-                    or (c == "'" and close == "'")
-                    or (c == ')' and close == ')')):
-                close = None
-                s_from = start
-                s_to = idx+1
-            elif c.isspace() and close == '\\s':
-                close = None
-                s_from = start
-                s_to = idx
-
-            # End of string. Force capture
-            if idx == len(search)-1:
-                close = None
-                s_from = start
-                s_to = idx+1
-
-            # We captured a term
-            if not close:
-                terms.append(search[s_from-backtrack:s_to])
-                start = idx+1
-                backtrack = 0
-                level = 1
-        else:
-            # We're looking for the beginning of a term
-            if c == '"':
-                close = '"'
-            elif c == "'":
-                close = "'"
-            elif c == '(':
-                close = ')'
-            elif c == '-':
-                backtrack += 1
-                continue
-            elif not c.isspace():
-                close = '\\s'
-
-            # We've started a capture
-            if close:
-                start = idx
-                if idx == len(search) - 1:
-                    # Capture started at end of string (single char term). Force capture.
-                    terms.append(search[start:idx+1])
-    return terms
-
-
-# Pick out only the terms that result in a search of field string content.
-# If the term is a (grouping) term, recursively extract terms from that too
-def extract_searchable_terms(terms):
-    extracted = []
-    for term in terms:
-        if term.lower().startswith('nc:'):
-            extracted.append(term[3:])
-        elif term[0] == '-':
-            pass
-        elif ':' in term:
-            pass
-        elif term.lower() == 'or' or term.lower() == 'and':
-            pass
-        elif term[0] == '(' and term[-1] == ')':
-            inner_terms = get_search_terms(term[1:-1])
-            extracted.extend(extract_searchable_terms(inner_terms))
-        elif term[0] in ["'", '"']:
-            if len(term) > 1:
-                # We will permit "and" and "or" as special cases
-                if term[1:-1].lower() in ['and', 'or']:
-                    extracted.append(term[1:-1])
-                else:
-                    extracted.extend(extract_searchable_terms([term[1:-1]]))
-        else:
-            extracted.append(term)
-    return extracted
-
 def _setup_view(self):
     self._view.setItemDelegate(FilterHighlightDelegate(self.browser, self._model))
 
-
-def on_webview_will_set_content(web_content: WebContent, context):
-    if not isinstance(context, Editor):
-        return
-    web_content.js.append(f"/_addons/{addon_package}/web/editor.js")
-
-def did_load_note(editor, focusTo=None) -> None:
-    if editor.editorMode is EditorMode.BROWSER:
-        # TODO: is there a better way to escape everything?
-        terms = json.dumps(browser_plus.filter_terms)
-        terms = terms.replace("'", r"\'")
-        terms = terms.replace("\\\\", r"\\\\")
-        terms = terms.replace("|", r"\\\\|")
-        editor.web.eval(f"bp_terms_str = '{terms}'")
-        editor.web.eval(f"bpParseTerms()")
-        editor.web.eval("bpHighlightAll()")
-
-
 mw.addonManager.setWebExports(__name__, r"web/.*")
 browser_plus = BrowserPlus(mw)
-
 
 # Hooks
 gui_hooks.browser_will_show.append(browser_plus._load)
 gui_hooks.browser_did_search.append(browser_plus.did_search)
 gui_hooks.browser_did_fetch_row.append(browser_plus._column_data)
-gui_hooks.webview_will_set_content.append(on_webview_will_set_content)
-gui_hooks.editor_did_load_note.append(did_load_note)
+gui_hooks.webview_will_set_content.append(browser_plus.on_webview_will_set_content)
+gui_hooks.editor_did_load_note.append(browser_plus.did_load_note)
 
 aqt.browser.Table._setup_view = wrap(aqt.browser.Table._setup_view, _setup_view)
---
+++ browserplus/utils/parser.py
@@ -0,0 +1,100 @@
+def parse_search(search):
+    terms = get_search_terms(search)
+    return extract_searchable_terms(terms)
+
+# Split search string by search terms
+def get_search_terms(search):
+    terms = []
+
+    start = 0
+    close = None
+    level = 1
+    backtrack = 0
+    idx = -1
+    for c in search:
+        idx += 1
+        # We're looking for a terminator
+        if close:
+            s_from = None
+            s_to = None
+
+            if close == ')' and c == '(':
+                level += 1
+                continue
+            if close == ')' and c == ')':
+                level -= 1
+                if level > 0:
+                    continue
+
+            if ((c == '"' and close == '"')
+                    or (c == "'" and close == "'")
+                    or (c == ')' and close == ')')):
+                close = None
+                s_from = start
+                s_to = idx+1
+            elif c.isspace() and close == '\\s':
+                close = None
+                s_from = start
+                s_to = idx
+
+            # End of string. Force capture
+            if idx == len(search)-1:
+                close = None
+                s_from = start
+                s_to = idx+1
+
+            # We captured a term
+            if not close:
+                terms.append(search[s_from-backtrack:s_to])
+                start = idx+1
+                backtrack = 0
+                level = 1
+        else:
+            # We're looking for the beginning of a term
+            if c == '"':
+                close = '"'
+            elif c == "'":
+                close = "'"
+            elif c == '(':
+                close = ')'
+            elif c == '-':
+                backtrack += 1
+                continue
+            elif not c.isspace():
+                close = '\\s'
+
+            # We've started a capture
+            if close:
+                start = idx
+                if idx == len(search) - 1:
+                    # Capture started at end of string (single char term). Force capture.
+                    terms.append(search[start:idx+1])
+    return terms
+
+
+# Pick out only the terms that result in a search of field string content.
+# If the term is a (grouping) term, recursively extract terms from that too
+def extract_searchable_terms(terms):
+    extracted = []
+    for term in terms:
+        if term.lower().startswith('nc:'):
+            extracted.append(term[3:])
+        elif term[0] == '-':
+            pass
+        elif ':' in term:
+            pass
+        elif term.lower() == 'or' or term.lower() == 'and':
+            pass
+        elif term[0] == '(' and term[-1] == ')':
+            inner_terms = get_search_terms(term[1:-1])
+            extracted.extend(extract_searchable_terms(inner_terms))
+        elif term[0] in ["'", '"']:
+            if len(term) > 1:
+                # We will permit "and" and "or" as special cases
+                if term[1:-1].lower() in ['and', 'or']:
+                    extracted.append(term[1:-1])
+                else:
+                    extracted.extend(extract_searchable_terms([term[1:-1]]))
+        else:
+            extracted.append(term)
+    return extracted
