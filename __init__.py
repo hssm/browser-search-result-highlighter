a2960c34ebea4be2654f13c73f4b99f44e73a84a
hssm <houssam.salem.au@gmail.com>
2024-12-27 23:11:39 +1100

    Remove attempt at table cell highlighting
    In preparation for the release. Will try again one day.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,11 @@
 import json
 
-from anki.hooks import wrap
 from aqt import *
 from aqt import gui_hooks
 from aqt.browser import SearchContext
 from aqt.editor import Editor, EditorMode
-from aqt.webview import WebContent
 from aqt.theme import theme_manager
+from aqt.webview import WebContent
 
 from utils.parser import parse_search
 
@@ -18,7 +17,7 @@ class QuickSearchAndHighlight:
         self.filter_terms = []
         self.last_search = ''
 
-    def _load(self, browser):
+    def browser_will_show(self, browser):
         self.browser = browser
         self.table = browser.table
         self.editor = browser.editor
@@ -28,18 +27,10 @@ class QuickSearchAndHighlight:
         # Drop-down selection
         self.browser.form.searchEdit.currentIndexChanged.connect(self.on_current_index_changed)
 
-
     def did_search(self, ctx: SearchContext):
         """Search has happened (regardless of source). Do highlight."""
         self.filter_terms = parse_search(ctx.search)
         print("qsah: Highlighting these terms: ", self.filter_terms)
-
-    def _column_data(self, item, is_notes_mode, row, active_columns):
-        c = self.table._state.get_card(item)
-        n = self.table._state.get_note(item)
-        for index, key in enumerate(active_columns):
-            #row.cells[index].text = "test text goes here"
-            pass
 
     def on_text_edited(self):
         """Textbox text has changed. Do a search."""
@@ -69,7 +60,7 @@ class QuickSearchAndHighlight:
             return
         web_content.js.append(f"/_addons/{addon_package}/web/editor.js")
 
-    def did_load_note(self, editor, focusTo=None) -> None:
+    def editor_did_load_note(self, editor, focusTo=None) -> None:
         if editor.editorMode is EditorMode.BROWSER:
             # TODO: is there a better way to escape everything?
             terms = json.dumps(qsah.filter_terms)
@@ -80,63 +71,13 @@ class QuickSearchAndHighlight:
             editor.web.eval(f"parseTerms()")
             editor.web.eval("beginHighlighter()")
 
-class FilterHighlightDelegate(QStyledItemDelegate):
-    def __init__(self, owner, choices):
-        super().__init__(owner)
-        self.items = choices
-        self.selection = QTextCharFormat()
-        self.selection.setBackground(QColor("#6c6435"))
-
-    # def sizeHint(self, option, index):
-    #     self.initStyleOption(option, index)
-    #     doc = QTextDocument()
-    #     doc.setHtml(option.text)
-    #     doc.setTextWidth(option.rect.width())
-    #     return QSize(doc.idealWidth(), doc.size().height())
-
-    def paint(self, painter, option, index):
-        if index.column() >= 0:  # todo: blacklist columns
-            self.initStyleOption(option, index)
-
-            # where = [(m.start(), m.end()) for m in re.finditer('the', txt)]
-            # style = option.widget.style()
-            # doc.setPlainText(option.text)
-            # doc.setDefaultFont(option.font)
-            # doc.setTextWidth(option.rect.width())
-            # doc.adjustSize()
-
-            doc = QTextDocument(index.model().data(index))
-            doc.setPageSize(QSizeF(option.rect.width(), option.rect.height()))
-
-            textOption = QTextOption()
-            textOption.setTextDirection(option.direction)
-            textOption.setAlignment(option.displayAlignment)
-            doc.setDefaultTextOption(textOption)
-
-            position = 0
-            while position >= 0:
-                cur = doc.find('the', position)
-                cur.setCharFormat(self.selection)
-                position = cur.position()
-            painter.save()
-            painter.translate(option.rect.x(), option.rect.y())
-            doc.drawContents(painter)
-            painter.restore()
-        else:
-            return QStyledItemDelegate.paint(self, painter, option, index)
-
-
-def _setup_view(self):
-    self._view.setItemDelegate(FilterHighlightDelegate(self.browser, self._model))
 
 mw.addonManager.setWebExports(__name__, r"web/.*")
 qsah = QuickSearchAndHighlight(mw)
 
 # Hooks
-gui_hooks.browser_will_show.append(qsah._load)
+gui_hooks.browser_will_show.append(qsah.browser_will_show)
 gui_hooks.browser_did_search.append(qsah.did_search)
-gui_hooks.browser_did_fetch_row.append(qsah._column_data)
 gui_hooks.webview_will_set_content.append(qsah.on_webview_will_set_content)
-gui_hooks.editor_did_load_note.append(qsah.did_load_note)
+gui_hooks.editor_did_load_note.append(qsah.editor_did_load_note)
 
-aqt.browser.Table._setup_view = wrap(aqt.browser.Table._setup_view, _setup_view)
