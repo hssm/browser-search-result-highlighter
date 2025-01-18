5d7ccc978f65334cfe1261d25b7ee9de882e5a8e
hssm <houssam.salem.au@gmail.com>
2025-01-19 10:14:37 +1100

    Elaborate parsing

--- utils/parser.py
+++ utils/parser.py
@@ -1,10 +1,10 @@
 def parse_search(search):
-    terms = get_search_terms(search)
+    terms = parse_nodes(search)
     return extract_searchable_terms(terms)
 
 
 # Split search string by search terms
-def get_search_terms(search):
+def parse_nodes(search):
     terms = []
 
     start = 0
@@ -33,11 +33,16 @@ def get_search_terms(search):
                 close = None
                 s_from = start
                 s_to = idx+1
+            if c == '"' and close == '\\s':
+                close = '"'
+                continue
+            if c == '(' and close == '\\s':
+                close = ')'
+                continue
             elif c.isspace() and close == '\\s':
                 close = None
                 s_from = start
                 s_to = idx
-
             # End of string. Force capture
             if idx == len(search)-1:
                 close = None
@@ -78,24 +83,75 @@ def get_search_terms(search):
 def extract_searchable_terms(terms):
     extracted = []
     for term in terms:
-        if term.lower().startswith('nc:'):
-            extracted.append(term[3:])
-        elif term[0] == '-':
-            pass
-        elif ':' in term:
-            pass
+        if not term:
+            continue
+        term = term.lower()
+        if term[0] == '-':
+            continue
         elif term.lower() == 'or' or term.lower() == 'and':
-            pass
+            continue
         elif term[0] == '(' and term[-1] == ')':
-            inner_terms = get_search_terms(term[1:-1])
+            inner_terms = parse_nodes(term[1:-1])
             extracted.extend(extract_searchable_terms(inner_terms))
+            continue
         elif term[0] in ["'", '"']:
             if len(term) > 1:
-                # We will permit "and" and "or" as special cases
-                if term[1:-1].lower() in ['and', 'or']:
-                    extracted.append(term[1:-1])
+                if ':' in term and term[term.index(':') - 1] != '\\':
+                    pass
                 else:
-                    extracted.extend(extract_searchable_terms([term[1:-1]]))
+                    if term[1:-1].lower() in ['and', 'or']:
+                        extracted.append({'tag': 'normal', 'term': term[1:-1]})
+                    elif term[1] == '(' and term[-2] == ')':
+                        extracted.append({'tag': 'quoted', 'term': term[1:-1]})
+                    else:
+                        if len(term) > 1 and term[0] == '"' and term[-1] == '"':
+                            term = term[1:-1]
+                        extracted.append({'tag': 'quoted', 'term': term})
+                    continue
+        if ':' in term and term[term.index(':') - 1] != '\\':
+            if len(term) > 1 and term[0] == '"' and term[-1] == '"':
+                term = term[1:-1]
+            prefix, main = term.split(':', 1)
+
+            if prefix in ignore:
+                continue
+            elif prefix == 'nc' or prefix == 'w': # Treat nc as a boundary. Should be enough for our case
+                if len(main) > 1 and main[0] == '"' and main[-1] == '"':
+                    main = main[1:-1]
+                extracted.append({'tag': 'boundary', 'term': main})
+            else:
+                extracted.append({'tag': 'field', 'field_name': prefix, 'term': extract_searchable_terms([main])})
+
         else:
-            extracted.append(term)
+            # Replace wildcards before adding
+            if '_' in term:
+                index = term.index('_')
+                if index == 0 or term[index - 1] != '\\':
+                    term = term.replace('_', '.')
+            if '*' in term:
+                index = term.index('*')
+                if index == 0 or term[index - 1] != '\\':
+                    term = term.replace('*', '.*')
+            if '\\:' in term:
+                term = term.replace('\\:', ':')
+
+            extracted.append({'tag': 'normal', 'term': term})
     return extracted
+
+ignore = ['tag', 'deck', 'preset', 'card', 'is', 'flag', 'prop', 'added', 'edited', 'rated', 'introduced', 'nid', 'cid']
+
+if __name__ == "__main__":
+    for search in [
+        "dog", "dog cat", "dog or cat", "dog (cat or mouse)", "-cat", "-cat -mouse", 'cat "and" mouse',
+        "-(cat or mouse)", "d_g", "d*g", "w:dog", "w:dog*", "w:d_g", "w:*dog", 'w:"and also"', 'nc:"and also"',
+        "front:dog", "front:*dog*", "front:",
+        "front:_*", "front:*", "fr*:text", "back:(cat or mouse -dog)", '"animal front:a dog"', '"a dog"', '-"a dog"',
+
+        # My tests
+        "d\\g", "_og", "*og", "do_", "do*", "_do\\", '\\"dog\\"', "\\dog\\", '"(text)"', '\\(text\\)', '"\\(text\\)"',
+        "w:3:30", "3\\:30",
+    ]:
+        # search = '"(text)"'
+        nodes = parse_nodes(search)
+        terms = extract_searchable_terms(nodes)
+        print(nodes, "\t | \t", terms)
